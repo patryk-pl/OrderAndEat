c2e91fec5b8b702e32f25a8f0d2b36299a19cbdb
patryk-pl <patrykbogdanski@outlook.com>
2020-11-20 14:34:57 +0100

    Create basic configuration
    Add areas, basic folders and simple route for pages.

--- OrderAndEat/Startup.cs
+++ OrderAndEat/Startup.cs
@@ -24,6 +24,7 @@ namespace OrderAndEat
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,7 +51,7 @@ namespace OrderAndEat
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
