5572e2b1df5d461a254c3a6406b256c1dec1fc35
patryk-pl <patrykbogdanski@outlook.com>
2020-11-23 22:30:39 +0100

    Add AutoMapper for Categories
    Add AutoMapper NuGet Package, Create CategoriesMapper

---
+++ OrderAndEat.Core/Mappers/CategoriesMapper.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using OrderAndEat.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderAndEat.Core.Mappers
+{
+    public class CategoriesMapper
+    {
+        private IMapper _mapper;
+
+        public CategoriesMapper()
+        {
+            _mapper = new MapperConfiguration(config =>
+            {
+                config.CreateMap<Category, CategoryDto>()
+                      .ReverseMap();
+            }).CreateMapper();
+        }
+
+        public CategoryDto Map(Category category) 
+            => _mapper.Map<CategoryDto>(category);
+
+        public IEnumerable<CategoryDto> Map(IEnumerable<Category> categories)
+            => _mapper.Map<IEnumerable<CategoryDto>>(categories);
+
+        public Category Map(CategoryDto category)
+            => _mapper.Map<CategoryDto>(category);
+
+        public IEnumerable<Category> Map(IEnumerable<CategoryDto> categories)
+            => _mapper.Map<IEnumerable<CategoryDto>>(categories);
+    }
+}
