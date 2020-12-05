8e7ab3ae9bc5461113e87387e38ecfd9e9f92c14
patryk-pl <patrykbogdanski@outlook.com>
2020-12-05 16:31:36 +0100

    Add MenuItem do DB
    Add MenuItem and MenuItem Table in DBContext and then make migration and update DB

---
+++ OrderAndEat.Database/Entities/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace OrderAndEat.Database
+{
+    public class MenuItem : BaseEntity
+    {
+        public string Description { get; set; }
+        public string Spicyness { get; set; }
+        public enum ESpicy { NA=0, NotSpicy=1, Spicy=2, VerySpicy=3 }
+        public string Image { get; set; }
+
+        [Display(Name="Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+
+        [Display(Name = "SubCategory")]
+        public int SubCategoryId { get; set; }
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price should be grater thab ${1}")]
+        public double Price { get; set; }
+    }
+}
--- OrderAndEat.Database/OrderAndEatDbContext.cs
+++ OrderAndEat.Database/OrderAndEatDbContext.cs
@@ -10,6 +10,7 @@ namespace OrderAndEat.Database
     {
         public DbSet<Category> Categories { get; set; }
         public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<MenuItem> MenuItems { get; set; }
         public OrderAndEatDbContext(DbContextOptions options) : base(options)
         {
 
